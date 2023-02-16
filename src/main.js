a209be18108ca2836660a8884225354a2c0b1b0a
Huy0110 <duchuy01102001@gmail.com>
2023-02-16 16:23:22 +0700

    push config

--- src/main.js
+++ src/main.js
@@ -1,17 +1,29 @@
+// import '@babel/polyfill'
+// import 'mutationobserver-shim'
 import Vue from 'vue'
+// import './plugins/bootstrap-vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
 import store from './store'
 import './plugins/base'
-import VueBarcode from "vue-barcode";
-Vue.use(VueBarcode)
-const axios = require('axios')
 
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+// // Import Bootstrap and BootstrapVue CSS files (order is important)
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(axios)
 Vue.config.productionTip = false
+
+Vue.mixin({
+    data: function() {
+      return {
+        globalOrder:[1,2]
+      }
+    }
+  })
 
 new Vue({
     vuetify,
